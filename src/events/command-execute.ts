0cc1ac82c9d069da846692577e852ac931030203
Ketz-dev <0hlowkey750@gmail.com>
2024-04-18 13:23:13 +0800

    idk

--- src/events/command-execute.ts
+++ src/events/command-execute.ts
@@ -1,4 +1,4 @@
-import Event from '../structures/Event'
+import { Event } from '../structures/Event'
 
 export default {
 	name: 'interactionCreate',
@@ -11,17 +11,21 @@ export default {
 
 		const command = client.commands.get(commandName)
 
-		if (!command) return await interaction.reply({ content: `Unknown command: \`${commandName}\`.`, ephemeral: true })
+		if (!command) {
+			await interaction.reply({ content: `Unknown command: \`${commandName}\`.`, ephemeral: true })
 
-		let { botPerms, name } = command
-
-		let me = await guild.members.fetchMe()
-
-		if (botPerms && !me.permissions.has(botPerms)) {
-			let missing = me.permissions.missing(botPerms)
-
-			return await interaction.reply({ content: `Missing permissions: ${missing.map(perm => `\`${perm.replace(/\B([A-Z])/g, ' $1')}\``)}.`, ephemeral: true })
+			return
 		}
+
+		let { name } = command
+
+		// let me = await guild.members.fetchMe()
+		//
+		// if (botPerms && !me.permissions.has(botPerms)) {
+		// 	let missing = me.permissions.missing(botPerms)
+		//
+		// 	return await interaction.reply({ content: `Missing permissions: ${missing.map(perm => `\`${perm.replace(/\B([A-Z])/g, ' $1')}\``)}.`, ephemeral: true })
+		// }
 
 		try {
 			await command.execute(client, interaction)
--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,80 +1,43 @@
-import { REST } from '@discordjs/rest'
-import { ActivityType, Client as BaseClient, Collection, IntentsBitField, Routes, Snowflake } from 'discord.js'
-import { readdir } from 'fs/promises'
-import path from 'path'
-import Command from './Command'
-import Event from './Event'
-import { Guild, PrismaClient } from '@prisma/client'
+import { ActivityType, Client as BaseClient, Collection, IntentsBitField } from 'discord.js'
+import { join } from 'path'
+import { readdirSync } from 'fs'
 
-export default class Client extends BaseClient<true> {
-	public readonly color = 0xf47333
+export class Client<T extends boolean = boolean> extends BaseClient<T> {
+	public readonly embedColor = 0xf47333
 
-	public readonly commands = new Collection<string, Command>()
-	public readonly prisma = new PrismaClient()
+	public readonly commands = new Collection<string, any>()
 
 	public constructor() {
 		super({
 			intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
-			allowedMentions: { repliedUser: false },
 			presence: {
-				activities: [{ name: 'for trouble...', type: ActivityType.Watching }]
+				activities: [
+					{
+						name: 'for trouble...',
+						type: ActivityType.Watching
+					}
+				]
 			}
 		})
 	}
 
-	public async getGuildModel(id: Snowflake): Promise<Guild> {
-		let guild = await this.prisma.guild.findUnique({
-			where: { id }
-		})
+	private loadCommands() {
+		const foldersPath = join(__dirname, 'commands')
+		const commandFolders = readdirSync(foldersPath)
 
-		return guild ?? await this.prisma.guild.create({
-			data: { id }
-		})
-	}
-
-	public async loadCommands() {
-		const categories = await readdir(path.join(__dirname, '../commands'))
-
-		for (const category of categories) {
-			const commandFiles = await readdir(path.join(__dirname, '../commands', category))
+		for (const folder of commandFolders) {
+			const filesPath = join(foldersPath, folder)
+			const commandFiles = readdirSync(filesPath)
+				.filter(file => file.endsWith('.js'))
 
 			for (const file of commandFiles) {
-				const command = require(path.join(__dirname, '../commands', category, file)).default as Command
+				const filePath = join(filesPath, file)
+				const command = require(filePath)
 
-				this.commands.set(command.name, command)
-
-				console.log(`Loaded command: ${command.name}`)
+				if (false) {
+					this.commands.set(command.name, command)
+				}
 			}
-		}
-
-		const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN!)
-
-		try {
-			await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: this.commands.map(Command.toJSON) })
-
-			console.log('Commands registered')
-		} catch (error) {
-			console.error('Failed to register commands:', error)
-		}
-	}
-	public async loadEvents() {
-		const eventFiles = await readdir(path.join(__dirname, '../events'))
-
-		for (const file of eventFiles) {
-			const event = require(path.join(__dirname, '../events', file)).default as Event<any>
-
-			this[event.once ? 'once' : 'on'](event.name, event.emit.bind(null, this))
-
-			console.log(`Loaded event: ${event.name}`)
-		}
-	}
-
-	public async start() {
-		try {
-			await this.prisma.$connect()
-			await this.login(process.env.BOT_TOKEN!)
-		} catch (error) {
-			console.error(error)
 		}
 	}
 }
